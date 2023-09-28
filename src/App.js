4a31315e6f2f2e8aace589282aebf6ec1e76b0f1
Danielewin8 <danielewin8@gmail.com>
2023-09-28 11:59:32 -0700

    Added tests for Spotify auth, and functionality for retrieving logged in user's spotify id

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import getUserId from './api/spotify';
 import './App.css';
 
 function App() {
@@ -32,7 +33,7 @@ function App() {
     // Check the URL hash for an access token
     const hash = window.location.hash;
     let token = window.localStorage.getItem('token');
-    
+
     if (!token && hash) {
       // Extract and set the token from the URL hash
       token = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1];
@@ -41,7 +42,14 @@ function App() {
     }
     // Set the token state
     setToken(token);
+    getUserId(token, setToken);
   }, []);
+
+  const logout = () => {
+    setToken("");
+    clearExistingTokens();
+    window.localStorage.removeItem("user_id");
+  }
 
   return (
     // Check if valid token is present, and adjust return accordingly
@@ -49,7 +57,10 @@ function App() {
       <header className="App-header">
         <h1>Spotify React</h1>
         {token ? (
-          <h2>You are logged in!</h2>
+          <div>
+            <h2>You are logged in!</h2>
+            <button onClick={logout}>Log out</button>
+          </div>
         ) : (
           <a onClick={handleSpotifyAuthClick} href="#/">Login to Spotify</a>
         )}
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+  // Mock the window.location object
+  delete window.location;
+  window.location = {
+    href: '',
+  };
+});
+
+describe('App component', () => {
+  it('renders the "Login to Spotify" button initially', () => {
+    const { getByText } = render(<App />);
+    const loginButton = getByText('Login to Spotify');
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it('redirects to the Spotify authentication URL when the login button is clicked', () => {
+    const { getByText } = render(<App />);
+    const loginButton = getByText('Login to Spotify');
+    fireEvent.click(loginButton);
+  });
+
+  it('handles successful authentication by setting the token', () => {
+    // Simulate successful authentication by setting a mock token in the URL hash
+    window.location.hash = '#access_token=mockAccessToken&token_type=Bearer';
+
+    render(<App />);
+
+    // Expect the component to render content for authenticated users
+    const authenticatedContent = screen.getByText('You are logged in!');
+    expect(authenticatedContent).toBeInTheDocument();
+
+    // Expect the token state to be set correctly
+    const token = localStorage.getItem('token');
+    expect(token).toBe('mockAccessToken');
+  });
+
+  it('logs out and removes the token', () => {
+    render(<App />);
+
+    const logoutButton = screen.getByText('Log out');
+    expect(logoutButton).toBeInTheDocument();
+
+    fireEvent.click(logoutButton);
+
+    expect(screen.getByText('Login to Spotify')).toBeInTheDocument();
+    expect(localStorage.getItem('token')).toBeNull();
+    expect(localStorage.getItem('user_id')).toBeNull();
+  });
 });
